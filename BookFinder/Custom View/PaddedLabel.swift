b9d32e6f70a9008cc0308057f32be62c92ba66f3
Ron Yau <hallol00111@gmail.com>
2025-05-20 03:16:37 +0800

    Working on search api load data

---
+++ BookFinder/Custom View/PaddedLabel.swift
@@ -0,0 +1,20 @@
+//
+//  PaddedLabel.swift
+//  BookFinder
+//
+//  Created by 邱允聰 on 20/5/2025.
+//
+
+import UIKit
+
+class PaddedLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
